d68a810c27575f1072c1fb4b3b17bab60b24d5ef
Crypoune <a.messenet@gmail.com>
2024-09-04 10:22:55 +0200

    j'ai fait plein de trucs

--- src/app.js
+++ src/app.js
@@ -1,14 +1,21 @@
 import "dotenv/config";
 import express from "express";
 import path from "path";
+
 import router from "./router/index.routes.js";
 
 const app = express();
 const PORT = process.env.LOCAL_PORT;
 
+// config moteur de template
 app.set("view engine", "ejs");
 app.set("views", path.join(process.cwd(), "src/view"));
+// config dossier statique
+app.use("/css", express.static(path.join(process.cwd(), "public/css")));
+app.use("/img", express.static(path.join(process.cwd(), "public/img")));
 
+
+// middleware pour le reouter
 app.use(router);
 
 
--- src/router/index.routes.js
+++ src/router/index.routes.js
@@ -2,8 +2,19 @@ import express from "express";
 
 const router = express.Router();
 
+// Route pour la page d'accueil
 router.get("/", (req, res) => {
     res.render("home");
+});
+
+// Route pour la page des news
+router.get("/news", (req, res) => {
+    res.render("news");
+});
+
+// Route pour afficher le formulaire de contact
+router.get("/contact", (req, res) => {
+    res.render("contact", { formData: null, error: null, success: null });
 });
 
 export default router;
